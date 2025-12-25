a3a3baf798e5db6b89f297ba1c9f5edd99598663
QuanDion <quan.ng@dion.vn>
2025-12-25 18:59:13 +0700

    commit lan hai

--- BE_122025.ConsoleApp/BE_122025.ConsoleApp/Program.cs
+++ BE_122025.ConsoleApp/BE_122025.ConsoleApp/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BE_122025.ConsoleApp
+{
+    using System;
+    using System.Threading.Tasks;
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            await Task.Delay(1000); // Simulate some async work
+            Console.WriteLine("Goodbye, World!");
+        }
+    }
+}
